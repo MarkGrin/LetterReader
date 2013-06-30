4f4649a907caebbaf8261d1d595b2dbc8a8ec799
MarkGrin <lew.ka@hotmail.com>
2013-06-30 04:18:16 -0700

    Added ChangeFileName to Logger/FileMethods.h

--- Logger/FileMethods.h
+++ Logger/FileMethods.h
@@ -2,5 +2,31 @@
 
 # define LOGGER_FILEMETHODS_H
 
+bool File :: ChangeFileName (const char* newName)
+{
+    if ( !newName )
+        return false;
+
+    unsigned int size = strlen (newName);
+
+    char* newPtr = nullptr;
+
+    try
+    {
+        newPtr = new char[size + 1];
+        memcpy (newPtr, newName, size);
+        newPtr[size] = 0;
+    }
+    catch (...)
+    {
+        return false;
+    }
+
+    if ( FileName_ )
+        delete[] FileName_;
+    FileName_ = newPtr;
+
+    return true;
+}
 
 # endif /* LOGGER_FILEMETHODS_H */
